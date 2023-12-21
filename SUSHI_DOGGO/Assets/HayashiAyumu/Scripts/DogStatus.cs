b863bd8ab021c62750ae9eba8c7cd1613c87513d
slugrain <ikishi1106@gmail.com>
2023-12-21 19:04:30 +0900

    ファイル名の重複を修正(仮)

--- SUSHI_DOGGO/Assets/HayashiAyumu/Scripts/DogStatus.cs
+++ SUSHI_DOGGO/Assets/HayashiAyumu/Scripts/DogStatus.cs
@@ -6,6 +6,7 @@ public class DogStatus : MonoBehaviour
 {
     public float _jumpPower = 300f;
     public float _movePower = 100f;
+    public float _moveTimer = 0.2f;
 
     public float _maxMoveLimit = 5f;
 }
