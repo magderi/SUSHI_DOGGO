66c1bc68753e0ca810f449c5ae592d2013076a9a
magderi <magderi9636@gmail.com>
2024-02-03 15:47:00 +0900

    かめらじゃぎ

--- SUSHI_DOGGO/Assets/Scripts/MainGameScripts/SalmonJumpJudgement.cs
+++ SUSHI_DOGGO/Assets/Scripts/MainGameScripts/SalmonJumpJudgement.cs
@@ -19,7 +19,6 @@ public class SalmonJumpJudgement : MonoBehaviour
     //接触したオブジェクトが引数otherとして渡される
     void OnTriggerStay(Collider other)
     {
-        //接触しているオブジェクトのタグが"Player"のとき
         if (other.CompareTag("Cloud") && _jumpCoolTime)
         {
             _sushiJump.isSalmonJumping = true;
@@ -31,7 +30,6 @@ public class SalmonJumpJudgement : MonoBehaviour
     //離れたオブジェクトが引数otherとして渡される
     void OnTriggerExit(Collider other)
     {
-        //離れたオブジェクトのタグが"Player"のとき
         if (other.CompareTag("Cloud"))
         {
             _sushiJump.isSalmonJumping = false;
